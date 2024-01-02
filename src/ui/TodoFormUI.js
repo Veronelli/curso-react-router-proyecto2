c799b5b3a36aff874ee2a6bdace4cae0a95c309b
Facundo Veronelli <fveronelli@gruposimpli.com>
2024-01-01 23:17:39 -0300

    add styling in add todo

--- src/ui/TodoFormUI.js
+++ src/ui/TodoFormUI.js
@@ -1,9 +1,13 @@
-import { TodoForm } from "../components/TodoForm"
+import { TodoForm } from "../components/TodoForm";
 
-function TodoFormUI({bottonLabel}){
-    return (
+function TodoFormUI({ bottonLabel }) {
+  return (
+    <>
+      <div style={{width:"100%",height:"100vh", display:"flex", justifyContent: "center", alignContent: "center"}}>
         <TodoForm />
-    )
+      </div>
+    </>
+  );
 }
 
-export default TodoFormUI
+export default TodoFormUI;
